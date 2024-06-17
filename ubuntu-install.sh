23af364927e1e0767a519b549c044557120bf682
kevin010717 <k511153362@gmail.com>
2024-06-17 13:32:28 +0800

    1

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -51,3 +51,7 @@ sudo docker run hello-world
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
+sudo sed -i 's/#HandleLidSwitch=suspend/HandleLidSwitch=ignore/' /etc/systemd/logind.conf
+sudo sed -i 's/#HandleLidSwitchExternalPower=suspend/HandleLidSwitchExternalPower=ignore/' /etc/systemd/logind.conf
+sudo systemctl restart systemd-logind.service
