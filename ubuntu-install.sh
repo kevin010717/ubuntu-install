faee95ac461ff54c3a2df3f294717a47f362c99d
kevin010717 <k511153362@gmail.com>
2024-06-17 11:18:31 +0800

    1

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+#markdown-preview.nvm
+#chatgpt
+#迁移到服务器
 sudo passwd root
 sudo passwd -u root
-su -
-
+sudo -i
 sudo apt update
-sudo apt install curl neovim git zsh net-tools tmux openssh-server -y
+sudo apt install curl neovim git gh zsh net-tools tmux openssh-server build-essential npm -y
 sh -c "$(curl -fsSL https://install.ohmyz.sh/)"
 sudo usermod -s /usr/bin/zsh ${whoami}
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/0xProto.zip
@@ -27,3 +29,25 @@ sudo chmod a+w /usr/lib/systemd/system/zerotier-one.service
 sudo sed -i '/^After.*/s/$/ shellcrash.service/' /usr/lib/systemd/system/zerotier-one.service
 sudo systemctl daemon-reload
 sudo zerotier-cli join 8056c2e21c28950a
+
+sudo apt install python3-pip python3-venv
+git clone https://github.com/janeczku/calibre-web.git .calibre-web
+cd .calibre-web
+python3 -m venv venv
+./venv/bin/python3 -m pip install -r requirements.txt
+nohup ./venv/bin/python3 cps.py
+
+curl -fsSL https://bun.sh/install | bash
+source .zshrc
+git clone https://github.com/gitbookIO/gitbook.git .gitbook
+cd .gitbook
+bun install
+bun dev
+
+sudo apt-get install docker.io
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo docker run hello-world
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.27.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
